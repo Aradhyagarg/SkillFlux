ef212efed9fb94cfcf22d93170194c96c8cc3818
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2023-12-12 09:15:00 +0530

    done in paymentController

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -66,42 +66,7 @@ export const getRazorPayKey = catchAsyncError(async(req, res, next) => {
     })
 }) 
 
-export const cancelSubscription = catchAsyncError(async (req, res, next) => {
-    const user = await User.findById(req.user._id);
-    const subscriptionId = user.subscription.id;
-  
-    let refund = false;
-  
-    await instance.subscriptions.cancel(subscriptionId);
-  
-    const payment = await Payment.findOne({
-      razorpay_subscription_id: subscriptionId,
-    });
-  
-    const gap = Date.now() - payment.createdAt;
-  
-    const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
-  
-    if (refundTime > gap) {
-      await instance.payments.refund(payment.razorpay_payment_id);
-      refund = true;
-    }
-  
-    await payment.deleteOne();
-    //   user.subscription.id = undefined;
-    //   user.subscription.status = undefined;
-    user.subscription = undefined;
-    await user.save();
-  
-    res.status(200).json({
-      success: true,
-      message: refund
-        ? `${user.name}: Your Subscription is now cancelled, You will recieve full refund within 7 days`
-        : `Subscription Cancelled, Now refund initiated as subscription was cancelled after 7 days.`,
-    });
-  });
-
-/*export const cancelSubscription = catchAsyncError(async(req, res, next) => {
+export const cancelSubscription = catchAsyncError(async(req, res, next) => {
     const user = await User.findById(req.user._id);
 
     const subscriptionId = user.subscription.id;
@@ -134,4 +99,3 @@ export const cancelSubscription = catchAsyncError(async (req, res, next) => {
         "Subscription cancelled, Now refund initiated as subscription was cancelled after 7 days"
     })
 })
-*/
